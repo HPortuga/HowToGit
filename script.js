4aa50bb906571ee7edb961146495fdd83ba14d2e
padilhaClaudio <padilha86@gmail.com>
2020-06-05 09:55:22 -0400

    "tasks 2"

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 var enterButton = document.getElementById("enter");
 var input = document.getElementById("userInput");
-var ul = document.getElementByTagName("ul");
+var ul = doucment.getElementByTagName("ul");
 var item = document.getElementsByTagName("li");
 
 var listaDeTarefas = [
   "Arrumar a casa",
   "limpar o banheiro",
-<<<<<<< HEAD
-<<<<<<< HEAD
   "Lavar o carro",
   "Tomar cerveja",
-=======
-=======
-  "Fazer uma tarefa qualquer",
-  "Trabalho de ED",
->>>>>>> c965b3c598ae163ff4f593eeaa0f10ffe7669c1e
   "dá uns bejo",
   "come",
->>>>>>> 63dfdbc3c7a05f28c55218cdd6961b3af54d85a5
   "fazer o rosh",
   "acender o carvão",
 ]
-
-
-function carregarLista() {
-  listaDeTarefas.forEach((item) => {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(item));
-    ul.appendChild(li);
-  });
-}
-
-
