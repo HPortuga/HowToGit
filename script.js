1fa3e3b57d910977dbd914ec299583d419cedb27
Matheus Henrique <mathsoares1@gmail.com>
2020-06-05 09:31:07 -0400

    alimentando lista de tarefas

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = doucment.getElementByTagName("ul");
+var item = document.getElementsByTagName("li");
+
 var listaDeTarefas = [
   "Arrumar a casa",
   "limpar o banheiro",
 ]
+
+function carregarLista() {
+  listaDeTarefas.forEach((item) => {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
+    ul.appendChild(li);
+  });
+}
